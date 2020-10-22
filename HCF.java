9d4edaa558a16b683098a9f003d2c64aad64c3ac
desibabua <kumarayush074@gmail.com>
2020-10-22 15:03:06 +0530

    added args length check to hcf program

--- HCF.java
+++ HCF.java
@@ -1,30 +1,21 @@
-/*
-
-1, 2 = 1
-2, 3 = 1
-2, 4 = 2
-4, 2 = 2
-
-0, 2 = 2
-2, 0 = 2
-
-*/
-
 public class HCF {
 
   public static void main(String[] args) {
-    int num1 = Integer.parseInt(args[0]);
-    int num2 = Integer.parseInt(args[1]);
+    String msg = "Arguments length not matched";
+    if (args.length == 2) {
+      int num1 = Integer.parseInt(args[0]);
+      int num2 = Integer.parseInt(args[1]);
+      int remainder = num2;
 
-    int remainder = num2;
-    while(remainder != 0){
-      remainder = num1 % num2;
-      num1 = num2;
-      num2 = remainder;
+      while (remainder != 0) {
+        remainder = num1 % num2;
+        num1 = num2;
+        num2 = remainder;
+      }
+
+      int hcf = num1;
+      msg = "HCF of " + args[0] + " and " + args[1] + " is " + hcf;
     }
-
-    int hcf = num1;
-    
-    System.out.println("HCF of " + args[0] + " and " + args[1] + " is " + hcf);
+    System.out.println(msg);
   }
 }
