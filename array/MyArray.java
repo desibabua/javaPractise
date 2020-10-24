a472de4a677e998d37c30f628339c87889cc9fc0
desibabua <kumarayush074@gmail.com>
2020-10-24 08:40:49 +0530

    added overload method for main to take step as arguments

--- array/MyArray.java
+++ array/MyArray.java
@@ -2,6 +2,8 @@ public class MyArray {
 
   public static void main(String[] args) {
     printArray(range(1, 5));
+    printArray(range(1, 10, 2));
+    printArray(range(5, 1, -1));
   }
 
   public static void printArray(int[] array) {
@@ -18,6 +20,17 @@ public class MyArray {
 
     for (int index = 0; index < range.length; index++) {
       range[index] = index + from;
+    }
+
+    return range;
+  }
+
+  public static int[] range(int from, int to, int step) {
+    int arrSize = (int) Math.ceil((to - from) / (double) step);
+    int[] range = new int[arrSize];
+
+    for (int index = 0; index < range.length; index++) {
+      range[index] = from + step * index;
     }
 
     return range;
